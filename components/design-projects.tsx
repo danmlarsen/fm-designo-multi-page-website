6c60dc417d68484210d5be8bdfd97a9a41710ead
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-04-11 21:31:03 +0200

    Added a few hover animations

--- components/design-projects.tsx
+++ components/design-projects.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 type TProject = {
   title: string;
@@ -11,12 +12,14 @@ export default function DesignProjects({ projects }: { projects: TProject[] }) {
     <section>
       <ul className="grid lg:grid-cols-3 gap-10">
         {projects.map(project => (
-          <li key={project.title} className="rounded-xl overflow-hidden bg-muted grid md:grid-cols-2 items-center lg:grid-cols-1">
-            <Image src={project.image} alt={project.title} />
-            <div className="text-center p-8 space-y-4">
-              <h2 className="text-primary font-medium text-xl uppercase tracking-[5px]">{project.title}</h2>
-              <p>{project.description}</p>
-            </div>
+          <li key={project.title} className="rounded-xl overflow-hidden bg-muted ">
+            <Link href="#" className="grid md:grid-cols-2 items-center lg:grid-cols-1 group">
+              <Image src={project.image} alt={project.title} className="group-hover:scale-105 transition-transform duration-300" />
+              <div className="text-center p-8 space-y-4 group-hover:bg-primary group-hover:text-white transition-colors duration-300">
+                <h2 className="text-primary font-medium text-xl uppercase tracking-[5px] group-hover:text-white">{project.title}</h2>
+                <p>{project.description}</p>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
--- components/designs-section.tsx
+++ components/designs-section.tsx
@@ -4,6 +4,8 @@ import ImageWebdesign from '@/assets/home/desktop/image-web-design-large.jpg';
 import ImageAppdesign from '@/assets/home/desktop/image-app-design.jpg';
 import ImageGraphicDesign from '@/assets/home/desktop/image-graphic-design.jpg';
 
+import IconRightArrow from '@/assets/shared/desktop/icon-right-arrow.svg';
+
 import Link from 'next/link';
 import Image, { type StaticImageData } from 'next/image';
 import { cn } from '@/lib/utils';
@@ -49,10 +51,21 @@ export default function Designs() {
 
 function DesignItem({ title, href, background, className }: React.ComponentProps<'a'> & { title: string; href: string; background: StaticImageData }) {
   return (
-    <Link href={href} className={cn('text-white', className)}>
-      <div className={cn(`grid place-items-center bg-cover rounded-lg h-[200px] md:h-[308px] relative bg-black/50 overflow-hidden`, className)}>
+    <Link href={href} className={cn('text-white group', className)}>
+      <div
+        className={cn(
+          `grid place-items-center bg-cover rounded-lg h-[200px] md:h-[308px] relative bg-black/50 overflow-hidden group-hover:bg-primary/75 transition-colors duration-300`,
+          className
+        )}
+      >
         <Image src={background} alt={title} fill className="-z-10 object-cover" />
-        <h3 className="text-5xl">{title}</h3>
+        <div className="flex flex-col items-center gap-6 uppercase font-medium">
+          <h3 className="text-5xl">{title}</h3>
+          <div className="text-[15px] tracking-[5px] flex items-center gap-5">
+            <span>View projects</span>
+            <Image src={IconRightArrow} alt="Right arrow" />
+          </div>
+        </div>
       </div>
     </Link>
   );
