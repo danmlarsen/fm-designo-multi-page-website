267de9d2efe6c180119f6ccb80977d17785736ef
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-04-12 23:26:03 +0200

    Minor adjustments

--- components/design-projects.tsx
+++ components/design-projects.tsx
@@ -13,9 +13,9 @@ export default function DesignProjects({ projects }: { projects: TProject[] }) {
       <ul className="grid lg:grid-cols-3 gap-10">
         {projects.map(project => (
           <li key={project.title} className="rounded-xl overflow-hidden bg-muted ">
-            <Link href="#" className="grid md:grid-cols-2 items-center lg:grid-cols-1 group">
+            <Link href="#" className="grid md:grid-cols-2 lg:grid-cols-1 group">
               <Image src={project.image} alt={project.title} className="group-hover:scale-105 transition-transform duration-300" />
-              <div className="text-center p-8 space-y-4 group-hover:bg-primary group-hover:text-white transition-colors duration-300">
+              <div className="text-center p-8 gap-4 group-hover:bg-primary group-hover:text-white transition-colors duration-300 flex flex-col justify-center">
                 <h2 className="text-primary font-medium text-xl uppercase tracking-[5px] group-hover:text-white">{project.title}</h2>
                 <p>{project.description}</p>
               </div>
--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Navigation from './navigation';
-import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer';
+import { Drawer, DrawerClose, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer';
 
 import IconHamburger from '@/assets/shared/mobile/icon-hamburger.svg';
 import IconClose from '@/assets/shared/mobile/icon-close.svg';
@@ -13,17 +13,18 @@ export default function MobileNav() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Drawer direction="top" open={isOpen} onOpenChange={open => setIsOpen(open)}>
-      <DrawerTrigger asChild>
-        <Button variant="ghost" className="md:hidden">
-          {!isOpen && <Image src={IconHamburger} alt="Hamburger icon" />}
-          {!!isOpen && <Image src={IconClose} alt="Close icon" />}
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent className="bg-black text-white px-6 py-12 data-[vaul-drawer-direction=top]:top-[120px] border-none data-[vaul-drawer-direction=top]:rounded-b-none">
-        <DrawerTitle hidden={true}>Mobile Navigation</DrawerTitle>
-        <Navigation className="gap-8 text-2xl items-start" />
-      </DrawerContent>
-    </Drawer>
+    <>
+      <Button variant="ghost" className="md:hidden z-[100] px-0" onClick={() => setIsOpen(!isOpen)}>
+        {!isOpen && <Image src={IconHamburger} alt="Hamburger icon" />}
+        {!!isOpen && <Image src={IconClose} alt="Close icon" />}
+      </Button>
+
+      <Drawer direction="top" open={isOpen} onOpenChange={open => setIsOpen(open)}>
+        <DrawerContent className="bg-black text-white px-6 py-12 data-[vaul-drawer-direction=top]:top-[95px] z-50 border-none data-[vaul-drawer-direction=top]:rounded-b-none">
+          <DrawerTitle hidden={true}>Mobile Navigation</DrawerTitle>
+          <Navigation className="gap-8 text-2xl items-start" />
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 }
